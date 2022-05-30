93e0e67c679753384918e1b2d99ad25bdaf69be7
Rohan00king <100908549+Rohan00king@users.noreply.github.com>
2022-05-30 10:29:00 +0530

    Update connection.py

--- Lumine/modules/connection.py
+++ Lumine/modules/connection.py
@@ -5,10 +5,10 @@ from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, Upda
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CommandHandler, CallbackQueryHandler, CallbackContext, run_async
 
-import AstrakoBot.modules.sql.connection_sql as sql
-from AstrakoBot import dispatcher, SUDO_USERS, DEV_USERS
-from AstrakoBot.modules.helper_funcs import chat_status
-from AstrakoBot.modules.helper_funcs.alternate import send_message, typing_action
+import Lumine.moduLumine.modules.sql.connection_sql les.sql.connection_sql as sql
+from Lumine import dispatcher, SUDO_USERS, DEV_USERS
+from Lumine.modules.helper_funcs import chat_status
+from Limine.modules.helper_funcs.alternate import send_message, typing_action
 
 user_admin = chat_status.user_admin
 
